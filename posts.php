e3a9b7f63899b213a4b30085e5d56bd64358d15b
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-10-31 22:42:11 +0800

    ..

--- posts.php
+++ posts.php
@@ -13,6 +13,7 @@
 <html>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <head>
+    <base target="_parent">
     <meta charset="utf-8">
     <title>posts</title>
 
@@ -130,11 +131,58 @@
                             页&nbsp;<button id="pager_go3" type="button" value="确定" style="padding: 0 2px 0 2px;height:21px;line-height:15px;">确定</button>&nbsp;</li>
                             <li class="l_reply_num">回复贴:<span class="red" style="margin-right:3px"><?php echo $THREAD['postnum'];?></span></li>
                           </ul>
+                            </div>
                           </div>
-                        </div>
-                    </div>
-                </div>
-              <iframe class="left-section" src="http://tieba.baidu.com/p/<?php echo $TID;?>" 
+                      </div>
+                  </div>
+
+              <style>
+                    .iframe{
+                  -webkit-background-clip: border-box;
+                  -webkit-background-origin: padding-box;
+                  -webkit-background-size: auto;
+                  -webkit-border-image: none;
+                  -webkit-box-shadow: rgb(192, 192, 192) 10px 10px 10px 0px;
+                  background-attachment: scroll;
+                  background-clip: border-box;
+                  background-color: rgb(255, 255, 255);
+                  background-image: none;
+                  background-origin: padding-box;
+                  background-size: auto;
+                  border-bottom-color: rgb(128, 128, 128);
+                  border-bottom-left-radius: 26.65625px;
+                  border-bottom-right-radius: 26.65625px;
+                  border-bottom-style: solid;
+                  border-bottom-width: 2px;
+                  border-left-color: rgb(128, 128, 128);
+                  border-left-style: solid;
+                  border-left-width: 2px;
+                  border-right-color: rgb(128, 128, 128);
+                  border-right-style: solid;
+                  border-right-width: 2px;
+                  border-top-color: rgb(128, 128, 128);
+                  border-top-left-radius: 26.65625px;
+                  border-top-right-radius: 26.65625px;
+                  border-top-style: solid;
+                  border-top-width: 2px;
+                  box-shadow: rgb(192, 192, 192) 10px 10px 10px 0px;
+                  color: rgb(0, 0, 0);
+                  display: block;
+                  font-family: 'DejaVu Sans', 'Bitstream Vera Sans', 'Lucida Sans', 'Hiragino Sans GB', 'Microsoft YaHei', 'WenQuanYi Micro Hei', sans-serif;
+                  font-size: 14px;
+                  height: 755px;
+                  margin-left: auto;
+                  margin-right: auto;
+                  margin-top: 30px;
+                  padding-bottom: 32px;
+                  /*padding-left: 64px;
+                  padding-right: 64px;
+                  */
+                  padding-top: 32px;
+                  text-align: justify;
+                  }
+              </style>
+              <iframe class="iframe" src="http://tieba.baidu.com/p/<?php echo $TID;?>" 
                   style="width:980px;height:900px;background:#808080;"
               ></iframe>
               </div>
