fb3809ae5d8ecd1b3f573b5b30298086567b17af
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-11-02 11:28:40 +0800

    ..

---
+++ author.php
@@ -0,0 +1,6 @@
+<td class="d_author">
+<div  style="height:80px;width:80px;padding:2px;border:2px solid #e0e0e0;margin-top:0px;margin-left:16px;">
+<img src="biguser.png" style="height:80px;width:80px;"/>
+</div>
+</td>
+
