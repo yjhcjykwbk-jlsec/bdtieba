2d15213c3f23eb64aa21f28481eeea63fd265891
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-11-02 22:43:00 +0800

    ..

--- mainpost.php
+++ mainpost.php
@@ -13,17 +13,12 @@
 <a class="l_post_anchor" name="40703980655l"></a>
 <div class="core_reply_tail">
 <ul class="p_reply">
-<li><a href="#" class="p_reply_first">回复</a>
-</li>
 </ul>
 <ul class="p_tail">
 <li><span class="">1楼</span>
 </li>
 <li><span><?php echo $POST['timestamp'];?></span>
 </li>
-</ul>
-<ul class="p_mtail">
-<li class="j_jb_ele" delete-data="{&quot;delete_mine&quot;:1,&quot;index&quot;:1,&quot;user_name&quot;:&quot;蜀山无影剑&quot;}" style=""><a href="#" class="jb_in post_del_href" data-checkun="un">删除</a>&nbsp;|</li>
 </ul>
 </div>
 </div>
