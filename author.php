ed53832d32d927771d1077d139f5cdd19ae263cb
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-11-07 23:15:42 +0800

    ..

--- author.php
+++ author.php
@@ -1,6 +1,6 @@
 <td class="d_author">
 <div  style="height:80px;width:80px;padding:2px;border:2px solid #e0e0e0;margin-top:0px;margin-left:16px;">
-<img src="user<?php echo rand(0,16);?>.jpg" style="height:80px;width:80px;"/>
+<img src="user<?php echo rand(0,$PHOTONUM);?>.jpg" style="height:80px;width:80px;"/>
 </div>
 </td>
 
