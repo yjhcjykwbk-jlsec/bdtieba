74c905c165dd68a1b8ec7660821ac32d23f864a3
stevenberge <zgxu2008@aliyun.com>
2014-04-25 03:19:36 +0800

    xx

--- author.php
+++ author.php
@@ -1,6 +1,6 @@
 <td class="d_author">
 <div  style="height:60px;width:60px;padding:2px;border:1px solid #e0e0e0;margin-top:0px;margin-left:16px;">
-<img src="user<?php echo rand(0,$PHOTONUM);?>.jpg" style="height:60px;width:60px;"/>
+<img src="user<?php echo rand(0,$PHOTONUM);?>.jpg" style="opacity:0.4;height:60px;width:60px;"/>
 </div>
 </td>
 
